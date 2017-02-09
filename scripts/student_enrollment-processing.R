edc38ebd14c72a24e450765f331559601619c685
jennadaly <jenna.m.daly@gmail.com>
2017-02-09 18:49:29 -0500

    Second commit, revised processing script.

--- scripts/student_enrollment-processing.R
+++ scripts/student_enrollment-processing.R
@@ -11,99 +11,63 @@
 #
 ##################################################################
 
-#rename 's/^/student-/' * #adds `student-` to the beginning of each filename
-#rename 's/\./_by-grade./g' *.csv -v #adds `_by-grade` to the end of each filename
+#terminal calls to run in local directory, to set up filenames for processing
+#rename 's/^/student-/' *                #adds `student-` to the beginning of each filename
+#rename 's/\./_by-grade./g' *.csv -v     #adds `_by-grade` to the end of each filename
 
-# library(plyr)
-# library(reshape2)
-
-setwd("/home/jdaly/Desktop/Data/Student Enrollment")
 sub_folders <- list.files()
 data_location <- grep("raw", sub_folders, value=T)
 path <- (paste0(getwd(), "/", data_location))
-raw_enrollment_data <- dir(path, pattern = ".csv")
+raw_data <- dir(path, pattern = ".csv")
+enrollment_data <- raw_data[grep("enrollment", raw_data)]
 
-data1filename <- file.path(getwd(), "raw", "student-enrollment2007-2008_by-grade.csv")
-data2filename <- file.path(getwd(), "raw", "student-enrollment2008-2009_by-grade.csv")
-data3filename <- file.path(getwd(), "raw", "student-enrollment2009-2010_by-grade.csv")
-data4filename <- file.path(getwd(), "raw", "student-enrollment2010-2011_by-grade.csv")
-data5filename <- file.path(getwd(), "raw", "student-enrollment2011-2012_by-grade.csv")
-data6filename <- file.path(getwd(), "raw", "student-enrollment2012-2013_by-grade.csv")
-data7filename <- file.path(getwd(), "raw", "student-enrollment2013-2014_by-grade.csv")
-data8filename <- file.path(getwd(), "raw", "student-enrollment2014-2015_by-grade.csv")
-data9filename <- file.path(getwd(), "raw", "student-enrollment2015-2016_by-grade.csv")
+#for loop steps
+##  reads in all the data into a data.frame, codes blank cells as NAs (accounts for rows where col# !=complete)
+##  sums the number of NAs in each row
+##  if (sum of NAs) > (max # of columns-2), removes rows (still allows districts where there is no data (NA for all grades))
+##  puts back header
+##  extracts year from name, adds year column
+##  removes district code column
+##  renames and saves new files
+#######################
+for (i in 1:length(enrollment_data)) {
+  current_file <- read.csv(file.path(path, enrollment_data[i]), header=F, skip=0,
+                           comment.char = "", check.names=T, stringsAsFactors=F, strip.white=TRUE, 
+                           na.strings=c("*", "", " ","NA"))
+  max_col <- ncol(current_file)
+  current_file<- current_file[!((rowSums(is.na(current_file)) > max_col-2)),]
+  colnames(current_file) = current_file[1, ]
+  current_file = current_file[-1, ]
+  get_year <- gsub(".*enrollment\\s*|_by.*", "", enrollment_data[i])
+  current_file$Year <- get_year 
+  current_file$"District Code" <- NULL
+  assign(paste0("data_", get_year), current_file)
+}
+######################
 
-filenames <- c("data1filename",
-               "data2filename",
-               "data3filename",
-               "data4filename",
-               "data5filename",
-               "data6filename",
-               "data7filename",
-               "data8filename",
-               "data9filename")
+#retrieves new files and binds them into one data.frame
+dfs <- ls()[sapply(mget(ls(), .GlobalEnv), is.data.frame)]
+get_data_only <- grep("^data", dfs, value=T)
 
-data1 <- read.csv(text=readLines(data1filename)[-(1:5)], header=T, stringsAsFactors=F, strip.white=TRUE, na.strings="*")
-data2 <- read.csv(text=readLines(data2filename)[-(1:5)], header=T, stringsAsFactors=F, strip.white=TRUE, na.strings="*")
-data3 <- read.csv(text=readLines(data3filename)[-(1:5)], header=T, stringsAsFactors=F, strip.white=TRUE, na.strings="*")
-data4 <- read.csv(text=readLines(data4filename)[-(1:5)], header=T, stringsAsFactors=F, strip.white=TRUE, na.strings="*")
-data5 <- read.csv(text=readLines(data5filename)[-(1:5)], header=T, stringsAsFactors=F, strip.white=TRUE, na.strings="*")
-data6 <- read.csv(text=readLines(data6filename)[-(1:5)], header=T, stringsAsFactors=F, strip.white=TRUE, na.strings="*")
-data7 <- read.csv(text=readLines(data7filename)[-(1:5)], header=T, stringsAsFactors=F, strip.white=TRUE, na.strings="*")
-data8 <- read.csv(text=readLines(data8filename)[-(1:5)], header=T, stringsAsFactors=F, strip.white=TRUE, na.strings="*")
-data9 <- read.csv(text=readLines(data9filename)[-(1:5)], header=T, stringsAsFactors=F, strip.white=TRUE, na.strings="*")
+data_all <- data.frame(stringsAsFactors=F)
 
-get_year1 <- gsub(".*enrollment\\s*|_by.*", "", data1filename)
-get_year2 <- gsub(".*enrollment\\s*|_by.*", "", data2filename)
-get_year3 <- gsub(".*enrollment\\s*|_by.*", "", data3filename)
-get_year4 <- gsub(".*enrollment\\s*|_by.*", "", data4filename)
-get_year5 <- gsub(".*enrollment\\s*|_by.*", "", data5filename)
-get_year6 <- gsub(".*enrollment\\s*|_by.*", "", data6filename)
-get_year7 <- gsub(".*enrollment\\s*|_by.*", "", data7filename)
-get_year8 <- gsub(".*enrollment\\s*|_by.*", "", data8filename)
-get_year9 <- gsub(".*enrollment\\s*|_by.*", "", data9filename)
-
-data1$Year <- get_year1 
-data2$Year <- get_year2 
-data3$Year <- get_year3 
-data4$Year <- get_year4 
-data5$Year <- get_year5 
-data6$Year <- get_year6 
-data7$Year <- get_year7 
-data8$Year <- get_year8 
-data9$Year <- get_year9 
-
-data1$District.Code <- NULL
-data2$District.Code <- NULL
-data3$District.Code <- NULL
-data4$District.Code <- NULL
-data5$District.Code <- NULL
-data6$District.Code <- NULL
-data7$District.Code <- NULL
-data8$District.Code <- NULL
-data9$District.Code <- NULL
-
-data_all <- rbind(data1,
-                  data2,
-                  data3,
-                  data4,
-                  data5,
-                  data6,
-                  data7,
-                  data8,
-                  data9)
+for (i in 1:length(get_data_only)) {
+  data_all <- rbind(data_all, get(get_data_only[i]))
+}
 
 # convert columns to appropriate classes
 data_all$District<-as.factor(data_all$District)
 data_all$Year<-as.factor(data_all$Year)
 data_all$Total<-as.integer(data_all$Total)
 
-colnames(data_all) <- c("District", "Pre Kindergarten", "Kindergarten", "1", "2", 
-                        "3", "4", "5", "6", "7", "8", 
+#rename columns
+colnames(data_all) <- c("District", "Pre Kindergarten", "Kindergarten", 
+                        "1", "2", "3", "4", "5", "6", "7", "8", 
                         "9", "10", "11", "12", "Total", "Year" )
 
-cols_to_stack <- c( "Pre Kindergarten", "Kindergarten", "1", "2", "3", "4", 
-                    "5", "6", "7", "8", 
+#reshape from wide to long format
+cols_to_stack <- c( "Pre Kindergarten", "Kindergarten", 
+                    "1", "2", "3", "4", "5", "6", "7", "8", 
                     "9", "10", "11", "12", "Total")
        
 long_row_count = nrow(data_all) * length(cols_to_stack)
@@ -117,19 +81,11 @@ long <- reshape(data_all,
                 direction = "long"
 )
 
+#merge in FIPS
 fips <- read.csv(paste0(path, "/", "school_district_ref.csv"), stringsAsFactors=F, header=T)
+enrollment_data_all <- merge(long, fips, by="District")
 
-setDT(long)
-setDT(fips)
-setkey(fips, `District`)
-setkey(long, `District`)
-enrollment_data_all <- join(long, fips, by="District")
-
-# unique(long$District)
-# new_DF <- enrollment_data_all[is.na(enrollment_data_all$FixedDistrict),]
-# unique(new_DF$District)
-
-##MISSING DISTRICTS
+##MISSING DISTRICTS from school_district_ref.csv
 # Eastern Connecticut Regional Educational Service Center (EASTCONN)     
 # Park City Prep Charter School District                            
 # Charter School for Young Children on Asylum Hill District              
@@ -139,19 +95,37 @@ enrollment_data_all <- join(long, fips, by="District")
 # Stamford Charter School for Excellence District                        
 # Capital Preparatory Harbor School Inc. District  
 
-setDT(enrollment_data_all)
-enrollment_data_all[, `:=`(
-  `Measure Type` = "Number",
-  Variable = "Student Enrollment"
-)]
+#add columns
+enrollment_data_all$"Measure Type" <- "Number"
+enrollment_data_all$"Variable" <- "Student Enrollment"
 
-enrollment_data_all$District = NULL
-setcolorder(enrollment_data_all, c("id", "FixedDistrict", "FIPS", "Year", "Grade", "Measure Type", "Variable", "Value"))
+#reorder/rename columns
+enrollment_data_all <- enrollment_data_all[c("id", "FixedDistrict", "District", "FIPS", "Year", "Grade", "Measure Type", "Variable", "Value")]
+enrollment_data_all[3] = NULL
 colnames(enrollment_data_all)[2] <- "District"
-enrollment_data_all <- enrollment_data_all[order(`District`, `Year`)]
 
+#order grade levels for sorting
+enrollment_data_all$Grade <- factor(enrollment_data_all$Grade, levels = cols_to_stack)
+
+#sort data
+enrollment_data_all <- enrollment_data_all[order(enrollment_data_all$District, enrollment_data_all$Year, enrollment_data_all$Grade),]
+
+#re-label "Total" as "Connecticut" and assign FIPS
 enrollment_data_all$District[enrollment_data_all$District %in% c(NA)] <- "Connecticut"
 enrollment_data_all$FIPS[enrollment_data_all$District %in% c("Connecticut")] <- 09
+
+#create data frame that calls out all entries with no data for enrollment
+value_NAs <- enrollment_data_all[is.na(enrollment_data_all$Value),]
+value_NAs$Grade <- factor(value_NAs$Grade, levels = c("Pre Kindergarten", "Kindergarten", "1", "2", "3", "4", 
+                                                      "5", "6", "7", "8", "9", "10", "11", "12", "Total"))
+value_NAs <- value_NAs[order(value_NAs$District, value_NAs$Year, value_NAs$Grade),]
+
+#recode no enrollment data with -9999
+enrollment_data_all[["Value"]][is.na(enrollment_data_all[["Value"]])] <- -9999
+
+#return blank in FIPS if not reported
+enrollment_data_all$FIPS <- as.character(enrollment_data_all$FIPS)
+enrollment_data_all[["FIPS"]][is.na(enrollment_data_all[["FIPS"]])] <- ""
 
 # Write to File
 write.table(
