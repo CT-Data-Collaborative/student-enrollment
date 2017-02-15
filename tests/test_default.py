ade51c2d5f01de64800c11db0e5531f5ebe00f42
jennadaly <jenna.m.daly@gmail.com>
2017-02-15 09:59:26 -0500

    Updated processing script, .json file, pytests, data passed tests.

--- tests/test_default.py
+++ tests/test_default.py
@@ -18,5 +18,5 @@ def test_spotcheck_testing(spotcheck_results):
     for check in spotcheck_results:
         assert check.expected == check.actual
 
-#def test_geography_count(geographies, geography_units_count):
-#    assert len(geographies) == geography_units_count
+def test_geography_count(geographies, geography_units_count):
+    assert len(geographies) == geography_units_count
